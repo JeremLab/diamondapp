0e15b550f716611bdd17ffe115c6367a746c297a
Jérémie Dupont <jeremie.dupont59@icloud.com>
2025-02-18 20:29:15 +0000

    Ajout du thème

--- DiamondApp/app.R
+++ DiamondApp/app.R
@@ -1,17 +1,11 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
+thematic_shiny(font = "auto")
+
 ui <- fluidPage(
-
+    theme = bs_theme(version = 5,
+                   bootswatch = "minty"),
     titlePanel("Explorations des Diamants"),
 
     sidebarLayout(
